a39f1421fbc579fee7ccb7dcd8ed888506a47add
shafiq98 <mohamed.shafiq98@gmail.com>
2021-10-12 20:55:00 +0800

    ControllerAdvisor -> Moved it from within Address folder (wrong place) to root of Exceptions Package
    User
    -> Modified return statement to return user's name instead of ID, will likely be better for displaying an error message for front end
    
    UserController
    -> removed todo since it has been completed

--- src/main/java/com/TakeMyMoney/service/exceptions/ControllerAdvisor.java
+++ src/main/java/com/TakeMyMoney/service/exceptions/ControllerAdvisor.java
@@ -1,15 +1,17 @@
-package com.TakeMyMoney.service.exceptions.addresses;
+package com.TakeMyMoney.service.exceptions;
 
 import com.TakeMyMoney.service.exceptions.BusinessExceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvisor{
     @ExceptionHandler(BusinessExceptions.class)
-    public ResponseEntity reponseMyException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ResponseBody
+    public ResponseEntity responseMyException(BusinessExceptions e) {
+        return new ResponseEntity<Object>(e.getMessage(),HttpStatus.BAD_REQUEST);
     }
 }
